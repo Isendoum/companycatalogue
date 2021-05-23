77bb60f95b82fa920c427bf4d1304dc6e80629cc
unknown <geotsek2003@yahoo.gr>
2021-05-23 22:25:24 +0300

    fix 4

--- src/main/java/com/backend/backend/configs/JPAConfig.java
+++ src/main/java/com/backend/backend/configs/JPAConfig.java
@@ -1,8 +1,11 @@
 package com.backend.backend.configs;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
 import org.springframework.dao.annotation.PersistenceExceptionTranslationPostProcessor;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
@@ -14,21 +17,26 @@ import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import javax.sql.DataSource;
+import java.util.Objects;
 import java.util.Properties;
 
 @Configuration
 @EnableJpaRepositories(basePackages = "com.backend.backend.repositories")
 @EnableTransactionManagement
+@PropertySource("classpath:application.properties")
 public class JPAConfig {
+    @Autowired
 
+    private Environment env;
     @Bean
     public DataSource dataSource() {
 
         DriverManagerDataSource dataSource = new DriverManagerDataSource();
-        dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
-        dataSource.setUsername("yourUsername");
-        dataSource.setPassword("yourPassword");
-        dataSource.setUrl("yourDatabaseLink?createDatabaseIfNotExist=true");
+
+        dataSource.setDriverClassName(env.getProperty("spring.datasource.driver-class-name"));
+        dataSource.setUsername(env.getProperty("spring.datasource.username"));
+        dataSource.setPassword(env.getProperty("spring.datasource.password"));
+        dataSource.setUrl(env.getProperty("spring.datasource.url"));
         return dataSource;
     }
 
